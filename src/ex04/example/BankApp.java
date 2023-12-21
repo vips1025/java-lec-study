6160a30c7f9add323f037dbb5d1029c47a502261
vips1025 <vips1025@gmail.com>
2023-12-21 14:41:00 +0900

    BankApp 완성

---
+++ src/ex04/example/BankApp.java
@@ -0,0 +1,39 @@
+package ex04.example;
+
+import ex04.example.model.Account;
+import ex04.example.model.User;
+
+public class BankApp {
+    public static void main(String[] args) {
+        // 1. 고객 2명 만들기
+        User ssar = new User(1, "ssar", "ssar@gmail.com");
+        User cos = new User(2, "cos", "cos@gmail.com");
+        User love = new User(3, "love", "love@gmail.com");
+
+        // 2. 계좌 2개 만들기
+        Account ssarAccount = new Account(1, 1000, 1);
+        Account cosAccount = new Account(2, 1000, 2);
+        Account loveAccount = new Account(3, 1000, 3);
+
+        // 3. 고객에게 정보를 받기(sender, receiver, amount)
+        long amount = 100L;
+
+        // 4. 이체(ssar -> cos 100원)
+        BankService.이체(ssarAccount, cosAccount, amount);
+
+        // 5. 이체(ssar -> love 100원)
+        BankService.이체(ssarAccount, loveAccount, amount);
+
+        // 6. 이체(cos -> love 100원)
+        BankService.이체(cosAccount, loveAccount, amount);
+
+        // 7. 객체 상태 확인
+        System.out.println(ssarAccount);
+        System.out.println(cosAccount);
+        System.out.println(loveAccount);
+
+        // 8. 출금
+        BankService.출금(ssarAccount, amount);
+        BankService.출금(loveAccount, amount);
+    }
+}
